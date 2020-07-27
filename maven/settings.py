6583a903ed51cfef423ce7d4d19d1d2bf3d1e8b7
NileshNandaniya <nileshnandaniya135@gmail.com>
2020-07-27 18:34:47 +0530

    Now user can update his password

--- maven/settings.py
+++ maven/settings.py
@@ -129,3 +129,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nileshnandaniya135@gmail.com'
+EMAIL_HOST_PASSWORD = '***********'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
