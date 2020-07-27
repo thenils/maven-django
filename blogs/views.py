00c78ea197ece50bc833af38297afdc2d015ad99
NileshNandaniya <nileshnandaniya135@gmail.com>
2020-07-27 16:49:39 +0530

    adding pagination to the postlistview and userpostlistview

--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 								  ListView,
@@ -35,7 +36,21 @@ class PostListView(ListView):
 	template_name = 'blogs/home.html'
 	#<app>/<model>_<viewtype>.html needed
 	context_object_name='posts'
-	ordering = ['-date_published'] 	
+	ordering = ['-date_published']
+	paginate_by = 5	
+
+
+class UserPostListView(ListView):
+	model = post
+	template_name = 'blogs/user_posts.html'
+	#<app>/<model>_<viewtype>.html needed
+	context_object_name='posts'
+	paginate_by = 5	
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return post.objects.filter(author=user).order_by('-date_published')
+
     
 class PostDetailView(DetailView):
 	model = post
@@ -73,4 +88,4 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 		return False
 
 def about(request):
-	return render(request,'blogs/about.html',{'title' : 'About'})
+	return render(request,'blogs/about.html',{'title' : 'Nilesh Nandaniya'})
